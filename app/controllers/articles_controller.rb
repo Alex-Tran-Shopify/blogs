f82006401f1d6b19a4b99f115408227b939eb15d
Alex-Tran-Shopify <97696996+Alex-Tran-Shopify@users.noreply.github.com>
2022-02-12 20:09:13 -0600

    added in create show index from scraps

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,16 @@ class ArticlesController < ActionController::Base
   def show
     @article = Article.find(params[:id])
   end
+  def index 
+    @articles = Article.all
+  end
+  def new 
+
+  end
+  def create 
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    # render plain: @article.inspect
+    redirect_to article_path(@article)
+  end
 end
